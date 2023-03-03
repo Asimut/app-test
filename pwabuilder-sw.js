bd158a11069a264c6db317476af40ffba3507b64
Asimut <asimut007@gmail.com>
2023-03-03 23:03:47 +0200

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,8 +1,18 @@
 // This is the "Offline page" service worker
-const CACHE = "pwabuilder-page";
+// const CACHE = "pwabuilder-page";
+const CACHE = "pwabuilder-offline-page";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "index.html";
+
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
@@ -11,37 +21,79 @@ self.addEventListener('install', async (event) => {
   );
 });
 
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-      .then(function(response){
-        console.log('PWA Builderadd page to offline cache: ' + response.url);
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-        event.waitUntil(updareChache(event.request, response.clone()));
+        if (preloadResp) {
+          return preloadResp;
+        }
 
-        return response;
-      })
-      .catch(function(error){
-        console.log('PWA Builder request failed. Serving content from cache: ' + error);
-        return fromCache(event.request);
-      })
-    );
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
   }
 });
 
-function fromCache(request){
-  return caches.open(CACHE).then(function(cache){
-    if(!matching || matching.status === 404){
-      return Promise.reject("no-match");
-    }
 
-    return matching;
-  })
-}
 
-function updareChache(request, response){
-  return caches.open(CACHE).then(function(cache){
-    return cache.put(request, response);
-  })
-}
+// self.addEventListener('install', async (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//   );
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith(
+//       fetch(event.request)
+//       .then(function(response){
+//         console.log('PWA Builderadd page to offline cache: ' + response.url);
+
+//         event.waitUntil(updareChache(event.request, response.clone()));
+
+//         return response;
+//       })
+//       .catch(function(error){
+//         console.log('PWA Builder request failed. Serving content from cache: ' + error);
+//         return fromCache(event.request);
+//       })
+//     );
+//   }
+// });
+
+// function fromCache(request){
+//   return caches.open(CACHE).then(function(cache){
+//     if(!matching || matching.status === 404){
+//       return Promise.reject("no-match");
+//     }
+
+//     return matching;
+//   })
+// }
+
+// function updareChache(request, response){
+//   return caches.open(CACHE).then(function(cache){
+//     return cache.put(request, response);
+//   })
+// }
