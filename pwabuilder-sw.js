9ec99672ad09d8eeaec52b139bd4e3ebee8d7c86
Asimut <asimut007@gmail.com>
2023-03-03 17:10:49 +0200

    Oleh

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,7 +1,7 @@
 // This is the "Offline page" service worker
-if( 'undefined' === typeof window){
-  importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-} 
+// if( 'undefined' === typeof window){
+//   importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// } 
 
 
 const CACHE = "pwabuilder-page";
