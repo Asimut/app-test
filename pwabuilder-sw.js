4481d0c1e5a2e290e47574aa93cd1de8999766a2
Asimut <asimut007@gmail.com>
2023-03-04 18:41:54 +0200

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -4,7 +4,7 @@ const CACHE = "pwabuilder-offline";
 
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');    
-importScripts('https://cdn.jsdelivr.net/npm/sw-toolbox@v6.1.1/dist/sw-toolbox.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "index.html";
